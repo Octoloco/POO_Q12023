c8fb001d305705894ff7af3777a2fcc5b141192f
AimeePB <106337619+AimeePB@users.noreply.github.com>
2023-01-24 17:32:54 -0600

    Add files via upload

---
+++ ProgramacionOrientadaAObjetos/Assets/AimeeNaomi/Tarea1/Scripts/PlayerAimee.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimee : MonoBehaviour
+{
+
+   
+   int Maxvidas = 3;
+   int Minvidas = 0;
+   int coins;
+   public float cronometro;
+   public GameObject Target;
+
+
+
+
+    
+        
+    
+}
