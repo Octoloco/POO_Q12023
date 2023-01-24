dacf1c4d8420be95dec8ec33f6e48b5aad770599
SalvaLDA <122999846+SalvaLDA@users.noreply.github.com>
2023-01-23 22:52:23 -0600

    Avance1

---
+++ ProgramacionOrientadaAObjetos/Assets/SalvadorPérez/Homework/Homework1/Scripts/PlayerSalvadorPerez.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSalvadorPerez : ActorSalvadorPerez
+{
+    int bombs = 0;
+    int mana = 10;
+    int coins = 10;
+    int stamina = 15;
+    int lives = 5;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
