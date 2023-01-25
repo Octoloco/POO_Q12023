170665348d4f986ebd58c15f1643a636053a6099
Map4chit0 <dchavezelizalde@gmail.com>
2023-01-25 15:50:22 -0600

    Daniela Chavéz

---
+++ ProgramacionOrientadaAObjetos/Assets/DanielaChavez/Homework/Homework1/Scripts/PlayerDanielaChavéz.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDanielaChavéz : ActorDanielaChavéz
+{
+    public int vida;
+    public int ataque;
+    public int defensa;
+    public int velocidad;
+    public int curación;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProgramacionOrientadaAObjetos/Assets/MarianaSalcedo/Tareas/Tarea1/ScriptsMarianaSalcedo/PlayerMarianaSalcedo.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMarianaSalcedo : ActorMarianaSalcedo
+{
+    private float force;
+    private int damage;
+    private int resistance;
+    protected float wisdom;
+    private int skill;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
