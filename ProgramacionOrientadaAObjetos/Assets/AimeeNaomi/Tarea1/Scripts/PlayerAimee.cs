0296690834a43661701d3057bd3efec3dbe42703
Map4chit0 <dchavezelizalde@gmail.com>
2023-01-25 15:51:13 -0600

    Daniela Chavéz

--- ProgramacionOrientadaAObjetos/Assets/AimeeNaomi/Tarea1/Scripts/PlayerAimee.cs
+++ ProgramacionOrientadaAObjetos/Assets/AimeeNaomi/Tarea1/Scripts/PlayerAimee.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,3 +20,26 @@ public class PlayerAimee : MonoBehaviour
         
     
 }
+=======
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAimee : MonoBehaviour
+{
+
+   
+   int Maxvidas = 3;
+   int Minvidas = 0;
+   int coins;
+   public float cronometro;
+   public GameObject Target;
+
+
+
+
+    
+        
+    
+}
+>>>>>>> 318769f2b17ddcfea1353612e54d0a7a8cba8c37
