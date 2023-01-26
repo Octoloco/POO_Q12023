4baa48d72fa122d138436a0b7d93088ed9099cfe
Andy Skywalker <106337873+AndySkywalker13@users.noreply.github.com>
2023-01-26 01:48:06 -0600

    Tarea1

---
+++ ProgramacionOrientadaAObjetos/Assets/AndreaGutierrez/Homework/Homework1/Scripts/ActorAndreaGutierrez.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorAndreaGutierrez : MonoBehaviour
+{
+    public int corazones;
+    public int daño;
+    private int salto;
+    protected float ataque;
+    protected float poderes;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProgramacionOrientadaAObjetos/Assets/AndreaGutierrez/Homework/Homework1/Scripts/PlayerAndreaGutierrez.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAndreaGutierrez : ActorAndreaGutierrez
+{
+    private float golpe;
+    private int muerte;
+    private int salto;
+    private int muertes;
+    protected float Ataque;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
