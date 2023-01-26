2fb8790cdb7e9bcd43315e81296629ae4866e5db
marmar1414 <123534916+marmar1414@users.noreply.github.com>
2023-01-26 01:24:19 -0600

    Tarea1NidiaEsperanza

---
+++ ProgramacionOrientadaAObjetos/Assets/EsperanzaNidia/TareaEsperanzaNidia/Tarea1EsperanzaNidia/Scripts/PlayerEsperanzaNidia.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEsperanzaNidia : MonoBehaviour
+{
+
+    string nombrePlayer;
+
+    int vida;
+
+    int fuerza;
+
+    int destreza;
+
+    int sabiduria;
+
+    void hablar()
+    {
+
+    }
+    void atacar()
+    {
+
+    }
+    void esquivar()
+    {
+
+    }
+    void volar()
+    {
+
+    }
+    void sanar()
+    {
+
+    }
+
+}
+public class ActorEsperanzaNidia : MonoBehaviour
+{
+    string nombreActor;
+
+    int espiritu;
+
+    int carisma;
+
+    int sabiduria;
+
+    int agilidad;
+
+    void pelear()
+    {
+
+    }
+    void escudarse()
+    {
+
+    }
+    void esconderse()
+    {
+
+    }
+    void regenerarse()
+    {
+
+    }
+    void powerup()
+    {
+
+    }
+}
+
+public class PlayerEsperanzaNidia : ActorEsperanzaNidia
+{
+
+}
