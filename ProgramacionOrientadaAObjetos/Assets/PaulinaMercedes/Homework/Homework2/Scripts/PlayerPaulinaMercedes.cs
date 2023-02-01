afe16a000ae13be115ad91db5e9812d758c8c7c3
PaulinaHrra <106337486+PaulinaHrra@users.noreply.github.com>
2023-01-31 19:48:43 -0600

    Actualización

--- ProgramacionOrientadaAObjetos/Assets/PaulinaMercedes/Homework/Homework2/Scripts/PlayerPaulinaMercedes.cs
+++ ProgramacionOrientadaAObjetos/Assets/PaulinaMercedes/Homework/Homework2/Scripts/PlayerPaulinaMercedes.cs
@@ -5,6 +5,28 @@ using UnityEngine;
 public class PlayerPaulinaMercedes
 
 {
+    //movimientos del jugador.
+    public float movimiento = 12f;
+    public float vida = 10;
+    public float fuerzaDeBrinco = 7;
+
+    //cosas que puede hacer el jugador.
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.forward * movimiento * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(-Vector3.forward * movimiento * Time.deltaTime);
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(-Vector3.right * movimiento * Time.deltaTime);
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.right * movimiento * Time.deltaTime);
+
+
+    }
+
+
     private string mezclarlosingredientes;
     private string seguirlospasosdelareceta;
     private string lavarlosingredientes;
@@ -15,7 +37,7 @@ public class PlayerPaulinaMercedes
     {
 
     }
-    void mezclarlosingredientes()
+    void mezclaringredientes()
     {
 
     }
