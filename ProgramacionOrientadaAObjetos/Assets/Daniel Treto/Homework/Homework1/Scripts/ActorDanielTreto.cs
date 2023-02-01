ec4bdc6415a08e72663c64c56f889f1a8fa71fca
ElChivote12 <106338353+ElChivote12@users.noreply.github.com>
2023-01-31 21:28:01 -0600

    Tarea2Treto

--- ProgramacionOrientadaAObjetos/Assets/Daniel Treto/Homework/Homework1/Scripts/ActorDanielTreto.cs
+++ ProgramacionOrientadaAObjetos/Assets/Daniel Treto/Homework/Homework1/Scripts/ActorDanielTreto.cs
@@ -2,15 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ActorDanielTreto : MonoBehaviour
+public class NewBehaviourScript : MonoBehaviour
 {
-
     public int vidas;
     public int items;
     public bool daño;
     public int fuerza;
     public bool velocidad;
-
 
     // Start is called before the first frame update
     void Start()
@@ -26,43 +24,4 @@ public class ActorDanielTreto : MonoBehaviour
     {
         
     }
-    /*----------------------------------------------------------------------------*/
-    public void Move()
-    {
-        transform.position += Vector3.right * velocidad * Time.deltaTime;
-    }
-
-    /*----------------------------------------------------------------------------*/
-
-    public void characterGrounded()
-    {
-        if (isGrounded) { Move(); }
-    }
-
-    /*----------------------------------------------------------------------------*/
-
-    public void hurt()
-    {
-        if (daño == true)
-        {
-            vidas--;
-        }
-    }
-
-    /*----------------------------------------------------------------------------*/
-
-    public void colectCoins()
-    {
-
-    }
-
-    /*----------------------------------------------------------------------------*/
-
-    public void heal()
-    {
-        if (items == 3) { vidas++; }
-    }
-}
-
-
 }
