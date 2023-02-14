0590e215b43f0849a162e527749c442efb55156d
Gabriel-bit31 <122992100+Gabriel-bit31@users.noreply.github.com>
2023-02-14 13:13:22 -0600

    .
    .

---
+++ ProgramacionOrientadaAObjetos/Assets/MarianaSalcedo/Tareas/Tarea1/ScriptsMarianaSalcedo/PlayerMarianaSalcedo.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMarianaSalcedo : ActorMarianaSalcedo
+{
+    //Exclusivo de 1 jugador.
+    private int distanciaSalto;
+    private int inventario;
+    public float coleccionables;
+    protected int energía;
+    [SerializeField] private int velocidad;
+
+    void Inventario()
+    {
+
+    }
+
+    void Empujar()
+    {
+
+    }
+
+    void Lanzar()
+    {
+
+    }
+
+    void Correr()
+    {
+
+    }
+
+    void Saltar()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
