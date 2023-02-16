88738b99bbdd95344d46921a9a421fba2e3e4712
CamiArtz <brandoncastroartworks20@gmail.com>
2023-02-15 20:50:41 -0600

    cambios

--- ProgramacionOrientadaAObjetos/Assets/BrandonAntonio/Homework/HomeWork2/PlayerBrandonAntonio.cs
+++ ProgramacionOrientadaAObjetos/Assets/BrandonAntonio/Homework/HomeWork2/PlayerBrandonAntonio.cs
@@ -2,58 +2,58 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBrandonAntonio : ActorBrandonAntonio
+// public class PlayerBrandonAntonio : ActorBrandonAntonio
 
-{
+//{
 
-    public Transform bulletSpawnPoint;
-    public GameObject bulletPrefab;
-    public float bulletSpeed = 10;
-    public float Contador;
+//   public Transform bulletSpawnPoint;
+//   public GameObject bulletPrefab;
+//    public float Contador;
+//    public float bulletSpeed = 10;
 
-    private int Vida;
+//   private int Vida;
 
-    private int Velocidad;
-    
-    private int JumpForce;
+//     private int Velocidad;
 
-    // Start is called before the first frame update
-    void Start()
-    {
+//      private int JumpForce;
 
-    }
-    // Update is called once per frame
-    void Update()
+// Start is called before the first frame update
+//  void Start()
+// {
 
-    {
-        if (Input.GetKey(KeyCode.W)) transform.Translate(Vector3(0, 1, 0) * Time.deltaTime * speed);
-        if (Input.GetKey(KeyCode.S)) transform.Translate(Vector3(0, -1, 0) * Time.deltaTime * speed);
-        if (Input.GetKey(KeyCode.D)) transform.Translate(Vector3(1, 0, 0) * Time.deltaTime * speed);
-        if (Input.GetKey(KeyCode.A)) transform.Translate(Vector3(-1, 0, 0) * Time.deltaTime * speed);
+// }
+// Update is called once per frame
+// void Update()
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
-            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
-        }
+// {
+//      if (Input.GetKey(KeyCode.W)) transform.Translate(Vector3(0, 1, 0) * Time.deltaTime * speed);
+//      if (Input.GetKey(KeyCode.S)) transform.Translate(Vector3(0, -1, 0) * Time.deltaTime * speed);
+//      if (Input.GetKey(KeyCode.D)) transform.Translate(Vector3(1, 0, 0) * Time.deltaTime * speed);
+//      if (Input.GetKey(KeyCode.A)) transform.Translate(Vector3(-1, 0, 0) * Time.deltaTime * speed);
 
-        Contador -= Time.deltaTime;
-        if (Contador <= 0.0f)
-        {
-            Destroy(gameObject);
+//        if (Input.GetKeyDown(KeyCode.Space))
+//        {
+//            var bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+//            bullet.GetComponent<Rigidbody>().velocity = bulletSpawnPoint.forward * bulletSpeed;
+//        }
 
-        }
+//        Contador -= Time.deltaTime;
+//        if (Contador <= 0.0f)
+//        {
+//            Destroy(gameObject);
+
+//     }
 
 
-       //private void OnCollisionEnter(Collision collision)
-        {
-         //   if (collision.collider.tag == "ENEMIGO")
-            {
-           //     Vida - 1;
-            }
-        }
+// private void OnCollisionEnter(Collision collision)
+// {
+//   if (collision.collider.tag == "ENEMIGO")
+//  {
+//      Vida - 1;
+//  }
+// }
 
-    }
-        
-}
+//  }
+
+//}
 
