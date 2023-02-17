c1b0f00fb3da96b392742687d5a4261cd41d125b
pau borja <paulina.borja35@my.unitec.edu.mx>
2023-02-17 11:05:59 -0600

    .

--- ProgramacionOrientadaAObjetos/Assets/PaulinaBorja/Homework/Homework2/Scripts/PlayerPaulinaBorja.cs
+++ ProgramacionOrientadaAObjetos/Assets/PaulinaBorja/Homework/Homework2/Scripts/PlayerPaulinaBorja.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Player_Paulina_Borja : ActorPaulinaBorja
+public class PlayerPaulinaBorja : ActorPaulinaBorja
 {
     public int food;
     public int bombs;
@@ -71,6 +71,7 @@ public class Player_Paulina_Borja : ActorPaulinaBorja
         }
     }
 
+    /*
     public void feed()
     {
 
@@ -102,4 +103,5 @@ public class Player_Paulina_Borja : ActorPaulinaBorja
     {
 
     }
+    */
 }
