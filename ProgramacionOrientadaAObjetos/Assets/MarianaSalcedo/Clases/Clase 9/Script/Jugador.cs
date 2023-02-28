0af99c1d7fbedfac2fd157fb941ab035d2fc0a8a
MariSalcedo <mariana.salcedo35@my.unitec.edu.mx>
2023-02-27 23:20:14 -0600

    .

--- ProgramacionOrientadaAObjetos/Assets/MarianaSalcedo/Clases/Clase 9/Script/Jugador.cs
+++ ProgramacionOrientadaAObjetos/Assets/MarianaSalcedo/Clases/Clase 9/Script/Jugador.cs
@@ -1,28 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.SceneManagement;
 
-public class Jugador : MonoBehaviour
-{
+//public class Jugador : MonoBehaviour
+//{
      
-    // Start is called before the first frame update
-    void Start()
-    {
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("pared"))
-        {
-            SceneManager.LoadScene(1);
+//    }
+//    private void OnCollisionEnter2D(Collision2D collision)
+//    {
+//        if (collision.gameObject.CompareTag("pared"))
+//        {
+//            SceneManager.LoadScene(1);
             
-        }
-    }
-}
+//        }
+//    }
+//}
